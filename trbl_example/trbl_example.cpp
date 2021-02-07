95295f075c5cbe4cc99238127c26e88524cb76e1
V.Telyatnikov <v.telyatnikov@xsolla.com>
2021-02-07 19:34:30 +0300

    Added gyro&accel print

--- trbl_example/trbl_example.cpp
+++ trbl_example/trbl_example.cpp
@@ -1,6 +1,12 @@
 #include "cube_api.h"
 #include <cstdio>
 
+struct Vec3f {
+    float x;
+    float y;
+    float z;
+} accel, gyro;
+
 class CEventLoopEx: public CEventLoop
 {
     uint8_t m_cid = 0;
@@ -15,24 +21,66 @@ protected:
             
             static char buf[64] = {};
             snprintf(buf, sizeof(buf), "DISPLAY: %d", display);
-            disp.DrawText(0, 0, buf, fColor(0, 0, 0), 1, 0);
+            disp.DrawText(0, 40, buf, fColor(0, 0, 0), 2, 0);
 
             snprintf(buf, sizeof(buf), "MODULE: %d", m_cid);
-            disp.DrawText(0, 60, buf, fColor(0, 0, 0), 1, 0);
+            disp.DrawText(0, 60, buf, fColor(0, 0, 0), 2, 0);
+
+            snprintf(buf, sizeof(buf), "GYRO:");
+            disp.DrawText(0, 80, buf, fColor(0, 0, 0), 2, 0);
+            
+            snprintf(buf, sizeof(buf), "X: %f", gyro.x);
+            disp.DrawText(0, 100, buf, fColor(0, 0, 0), 2, 0);
+
+            snprintf(buf, sizeof(buf), "Y: %f", gyro.y);
+            disp.DrawText(0, 120, buf, fColor(0, 0, 0), 2, 0);
+
+            snprintf(buf, sizeof(buf), "Z: %f", gyro.z);
+            disp.DrawText(0, 140, buf, fColor(0, 0, 0), 2, 0);
+
+            snprintf(buf, sizeof(buf), "ACCL");
+            disp.DrawText(0, 160, buf, fColor(0, 0, 0), 2, 0);
+
+            snprintf(buf, sizeof(buf), "X: %f", accel.x);
+            disp.DrawText(0, 180, buf, fColor(0, 0, 0), 2, 0);
+            
+            snprintf(buf, sizeof(buf), "Y: %f", accel.y);
+            disp.DrawText(0, 200, buf, fColor(0, 0, 0), 2, 0);
+
+            snprintf(buf, sizeof(buf), "Z: %f", accel.z);
+            disp.DrawText(0, 220, buf, fColor(0, 0, 0), 2, 0);
 
             disp.DrawLine(120, 0, 120, 120, fColor(1, 0, 0));
             disp.DrawLine(0, 120, 120, 120, fColor(0, 1, 0));
-
         }
         return CEventLoop::OnTick(time);
     }
 
+    
+    void OnGyroChanged(const Get_Gyro_1_0& _gyro) {
+        NativePrint("GYRO CHANGED");
+        gyro.x = _gyro.axis_X;
+        gyro.y = _gyro.axis_Y;
+        gyro.z = _gyro.axis_Z;
+        NativePrint("GYRO: X:%f, Y:%f, Z:%f", gyro.x, gyro.y, gyro.z);
+    }
+    void OnAccelChanged(const Get_Accel_1_0& _accel) {
+        NativePrint("ACCEL CHANGED");
+        accel.x = _accel.axis_X;
+        accel.y = _accel.axis_Y;
+        accel.z = _accel.axis_Z;
+        NativePrint("ACCEL: X:%f, Y:%f, Z:%f", accel.x, accel.y, accel.z);
+    }
 
     void OwnCID(uint8_t cid) override
     {
         NativePrint("MY CID IS %d", cid);
         m_cid = cid;
     }
+
+    virtual void OnMessage(uint32_t size, const Get_Message_1_0& msg){
+        NativePrint("WASM OnMessage from: %d, size: %d, message: %s", msg.from_cid, msg.size, (char*)msg.data);
+    };
    
     virtual void OnTRBLChanged(const Get_TRBL_1_0& trbl) {
         NativePrint("on trbl changed \n");
