76f6001c35c4bb3b2a2097a9feaea3c701c7df82
Alex <araud@mail.ru>
2021-03-08 17:56:16 -0800

    steps toward wrapping life around cube

--- life/life.cpp
+++ life/life.cpp
@@ -9,20 +9,19 @@
 const int g = 10;
 const int k = 240 / g;
 using TLife = char[k*k];
-TLife life[3] = {};
+TLife life[8][3] = {};
 
 
 class CEventLoopEx: public CEventLoop
 {
-    int m_nPos = 0;
+
     uint32_t m_nPrevTime = 0;
-    int m_grad = 0;
     Get_TRBL_1_0 m_trbl = {};
     CCubeGraph m_cg;
-    CBitmap m_ball;
     uint8_t m_cid = 0xFF;
-    std::unique_ptr<CScubo> m_spCrossGeo;
-
+    struct sync_t {
+        unsigned char finished;
+    } m_sync;
 
     size_t print_transpon(const uint8_t(&matrix)[8][3], char* buffer) {
         uint8_t result[3][8];
@@ -100,62 +99,22 @@ protected:
         return std::make_tuple(x, y);
     }
 
-    void CrossGeo(CDisplay& disp)
-    {
-        if (0xFF == m_cid || m_trbl.CID[0][0] == m_trbl.CID[1][1]) //not set yet
-            return;
-        if (m_cid == 0 && disp.Index() == 0)
-        {
-            int x, y;
-            std::tie(x, y) = Circle(0, 0, 150, m_grad);
-            CScubo pos(m_trbl, x, y, 0, m_cid);
-            if (pos.cid() == m_cid)
-                m_spCrossGeo = std::make_unique<CScubo>(m_trbl, pos.pack());
-            else {
-                NativeSendStruct(estAll, pos.pack());
-                return;
-            }
-        }
-        if (!m_spCrossGeo || m_spCrossGeo->cid() != m_cid || m_spCrossGeo->disp() != disp.Index())
-            return;
-        disp.FillCircle(m_spCrossGeo->x(), m_spCrossGeo->y(), 50, fColor(1, 1, 1));
-    }
 
     bool OnTick(uint32_t time) override
     {
-        int x, y;
-        std::tie(x, y) = Circle(240, 240, 150, m_grad);
-        CScuboSprite ball(m_trbl, x - 50, y - 50, 100, 100);
-        //NativePrint("%d:%d:%d->%d:%d:%d", 90 + m_grad, x, y, scb.disp(), scb.x(), scb.y());
-        std::tie(x, y) = Circle(x, y, 75, m_grad);
-        CScubo small_circle(m_trbl, x, y);
-        ++m_grad %= 270;
+        if (0xFF == m_cid || m_trbl.CID[0][0] == m_trbl.CID[1][1] || m_sync.finished != 0xFF) //not set yet
+            return true;
+        m_sync.finished = 0;
+
+        UpdateLife();
 
         for (int display = 0; display < 3; ++display)
         {
             CDisplay disp(display);
             disp.Fill(fColor(1,1,1));
-#if 1
 
-            UpdateLife(disp);
+            DrawLife(disp);
 
-            if (display == 1)
-            {
-                ++m_nPos %= 240;
-                disp.DrawText(120, 120, "Life!", fColor(1,1,1), 30, m_nPos % 360);
-                static char buf[64] = {};
-                snprintf(buf, sizeof(buf), "accel: %.2f %.2f %.2f", m_accel.axis_X, m_accel.axis_Y, m_accel.axis_Z);
-                disp.DrawText(0, 20, buf, fColor(0,1,0), 2, 0);
-            }
-
-            if (display == 2)
-            {
-                disp.DrawLine(0,0,240,240, 100);
-                disp.FillRect(m_nPos, m_nPos, 240, 10, fColor(0,1,0));
-                disp.DrawPixelAlpha(66, 66, 255, 2);
-            }
-
-#endif
             static char buf[64] = {};
             //snprintf(buf, sizeof(buf), "D:%d", display);
 
@@ -166,22 +125,6 @@ protected:
 
             disp.DrawText(0, 100, buf, fColor(0, 0, 0), 6, 0);
 
-#if 0
-            CScubo tl = ball.TopLeft();
-            CScubo br = ball.BottomRight();
-            if (tl.disp() == display)
-                disp.DrawBitmap(tl.x(), tl.y(), m_ball, 1, 360 - m_grad);
-            else if (br.disp() == display)
-                disp.FillCircle(br.x(), br.y(), 10, fColor(1, 0, 1));
-                //disp.DrawBitmap(br.x() - 100, br.y() - 100, m_ball, 1, 360 - m_grad);
-#else
-            CScubo big_circle = ball.Center();
-            if (big_circle.disp() == display)
-                disp.DrawBitmap(big_circle.x(), big_circle.y(), m_ball, 1, 360 - m_grad);
-            if (small_circle.disp() == display)
-                disp.FillCircle(small_circle.x(), small_circle.y(), 20, fColor(1, 0, 1));
-#endif
-
             static char fps[32] = {};
             if (m_nPrevTime && display == 0)
             {
@@ -191,9 +134,9 @@ protected:
             disp.DrawText(0, 0, fps, fColor(1, 1, 1), 3);
             m_nPrevTime = time;
 
-            //NativePrint("Draw for display %d, time: %d\n", display, time);
-            CrossGeo(disp);
         }
+        m_sync.finished |= 1 << m_cid;
+        NativeSendStruct(estAll, m_sync);
         return CEventLoop::OnTick(time);
     }
 
@@ -204,67 +147,86 @@ protected:
 
     void Randomize()
     {
-        for (int i = 0; i < 3; ++i)
-            for (int y = k * k; y--;)
-                life[i][y] = rand() % 2;
+        for (int cid = 0; cid < 8; ++cid)
+            for (int disp = 0; disp < 3; ++disp)
+                for (int y = k * k; y--;)
+                    life[cid][disp][y] = rand() % 2;
+
+        uint32_t len = sizeof(life) + sizeof(intptr_t);
+        auto buff = GetSharableMem(len);
+        intptr_t* ptr = reinterpret_cast<intptr_t*>(buff.get());
+        *ptr = unique_type_id<TLife>();
+        memcpy(ptr + 1, life, sizeof(life));
+        NativeInvoke(Send_Message_1_0{ estAll, len, buff.get() });
     }
 
-    void UpdateLife(CDisplay& disp) {
-        static uint16_t colors[] = { fColor(1, 0, 0), fColor(0, 0, 1), fColor(0, 1, 0), fColor(0, 0, 0) };
-        TLife& old = life[disp.Index()];
-        char c[k*k] = {};
+    void DrawLife(CDisplay& disp) {
+        TLife& old = life[m_cid][disp.Index()];
+
         for (int i = k * k; i--;) {
-            int n = 0;
             int x = i % k;
             int y = i / k;
-            //n += b[(i / k + k + f % 3 - 1) % k * k + (i + k + f / 3 - 1) % k];
             Draw(disp, x * g, y * g, g - 1, g - 1, old[i]);
-
-            for (int f = 9; f--;)
-            {
-                int dx = f % 3 - 1;
-                int dy = f / 3 - 1;
-                int nx = x + dx;
-                int ny = y + dy;
-
-                if (nx < 0 || ny < 0)
-                {
-                    //other module
-                }
-                else
-                {
-                    CScuboT<k> scb(m_trbl, nx, ny, disp.Index());
-                    n += life[scb.disp()][scb.y() * k + scb.x()];
-                }
-            }
-
-            c[i] = (n == 3) || (n - old[i] == 3);
         }
+    }
 
-        if (0 != memcmp(old, c, sizeof(old)))
-            memcpy(old, c, sizeof(old));
-        else
-            Randomize();
+    void UpdateLife() {
+        //static uint16_t colors[] = { fColor(1, 0, 0), fColor(0, 0, 1), fColor(0, 1, 0), fColor(0, 0, 0) };
+        for (int cid = 0; cid < 8; ++cid)
+            for (int disp = 0; disp < 3; ++disp)
+            {
+                TLife& old = life[cid][disp];
+                char c[k * k] = {};
+                for (int i = k * k; i--;) {
+                    int n = 0;
+                    int x = i % k;
+                    int y = i / k;
+
+                    for (int f = 9; f--;)
+                    {
+                        int dx = f % 3 - 1;
+                        int dy = f / 3 - 1;
+                        int nx = x + dx;
+                        int ny = y + dy;
+
+                        CScuboT<k> scb(m_trbl, nx, ny, disp, cid);
+                        n += life[scb.cid()][scb.disp()][scb.y() * k + scb.x()];
+                    }
+
+                    c[i] = (n == 3) || (n - old[i] == 3);
+                }
+                if (0 != memcmp(old, c, sizeof(old)))
+                    memcpy(old, c, sizeof(old));
+                else if (m_cid == 0)
+                    Randomize();
+            }
     }
 
     void OnMessage(uint32_t size, const Get_Message_1_0& msg) override
     {
-        WC_CHECKRET(*(uintptr_t*)msg.data == unique_type_id<CScubo::pack_t>(), WC_NORET);
-        const CScubo::pack_t& pack = *reinterpret_cast<const CScubo::pack_t*>((uintptr_t*)msg.data + 1);
-        m_spCrossGeo = std::make_unique<CScubo>(m_trbl, pack);
+        if (*(uintptr_t*)msg.data == unique_type_id<sync_t>())
+        {
+            const sync_t& sync = *reinterpret_cast<const sync_t*>((uintptr_t*)msg.data + 1);
+        }
+        else if (*(uintptr_t*)msg.data == unique_type_id<TLife>())
+        {
+            memcpy(life, (uintptr_t*)msg.data + 1, sizeof(life));
+            m_sync.finished = 0xFF;
+        }
     }
 
     void OwnCID(uint8_t cid) override
     {
         m_cid = cid;
         NativePrint("MY CID IS %d", cid);
+        if (m_cid == 0)
+            Randomize();
     }
 
 public:
     int Main() override
     {
         NativePrint("Hello WOWd\n");
-        Randomize();
         NativeInvoke( Send_Message_1_0{ estSelf, 0, NULL} );
 
         return CEventLoop::Main();
